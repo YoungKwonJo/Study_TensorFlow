4de9662c71c1a4824d1f5289c679c53e9994e4db
YoungKwon Jo <Young-Kwon.Jo@cern.ch>
2017-01-16 17:10:10 +0000

    add summary in CNN.py

--- 08 - CNN/CNN.py
+++ 08 - CNN/CNN.py
@@ -6,8 +6,8 @@ import input_data
 batch_size = 128
 test_size = 256
 
-def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+def init_weights(shape,name):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01),name)
 
 
 def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
@@ -34,6 +34,7 @@ def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
     l4 = tf.nn.dropout(l4, p_keep_hidden)
 
     pyx = tf.matmul(l4, w_o)
+    #print("model:",pyx)
     return pyx
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -44,36 +45,48 @@ teX = teX.reshape(-1, 28, 28, 1)  # 28x28x1 input img
 X = tf.placeholder("float", [None, 28, 28, 1])
 Y = tf.placeholder("float", [None, 10])
 
-w2 = init_weights([3, 3, 32, 64])     # 3x3x32 conv, 64 outputs
-w = init_weights([3, 3, 1, 32])       # 3x3x1 conv, 32 outputs
-w3 = init_weights([3, 3, 64, 128])    # 3x3x32 conv, 128 outputs
-w4 = init_weights([128 * 4 * 4, 625]) # 128의 필터에 4 * 4 이미지
-w_o = init_weights([625, 10])         # FC 625 inputs, 10 outputs (labels)
+w2 = init_weights([3, 3, 32, 64],"w2")     # 3x3x32 conv, 64 outputs
+w = init_weights([3, 3, 1, 32],"w")       # 3x3x1 conv, 32 outputs
+w3 = init_weights([3, 3, 64, 128],"w3")    # 3x3x32 conv, 128 outputs
+w4 = init_weights([128 * 4 * 4, 625],"w4") # 128 filters * 4*4 image
+w_o = init_weights([625, 10],"w_o")         # FC 625 inputs, 10 outputs (labels)
 
 p_keep_conv = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
 py_x = model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+with tf.name_scope('cost') as scope:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+    cost_summ = tf.scalar_summary("cost", cost)
+with tf.name_scope('train') as scope:
+    train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+
 predict_op = tf.argmax(py_x, 1)
+
+w_o_hist = tf.histogram_summary("w_o", w_o)
+
 
 # Launch the graph in a session
 with tf.Session() as sess:
 
     tf.initialize_all_variables().run()
-
-    for i in range(100):
+    merged = tf.merge_all_summaries()
+    writer = tf.train.SummaryWriter("./logs/xor_logs", sess.graph)
+    iii=0
+    for i in range(3):
         training_batch = zip(range(0, len(trX), batch_size),
                              range(batch_size, len(trX), batch_size))
 
         for start, end in training_batch:
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
                                           p_keep_conv: 0.8, p_keep_hidden: 0.5})
-
-            print sess.run(cost, feed_dict={X: trX[start:end], Y: trY[start:end],
-                                            p_keep_conv: 0.8, p_keep_hidden: 0.5})
-
+            if iii % 100 == 0:
+                summary = sess.run(merged, feed_dict={X: trX[start:end], Y: trY[start:end],
+                                          p_keep_conv: 0.8, p_keep_hidden: 0.5})
+                writer.add_summary(summary, iii)
+            #print sess.run(cost, feed_dict={X: trX[start:end], Y: trY[start:end],
+            #                                p_keep_conv: 0.8, p_keep_hidden: 0.5})
+            iii+=1
         test_indices = np.arange(len(teX)) # Get A Test Batch
         np.random.shuffle(test_indices)
         test_indices = test_indices[0:test_size]
@@ -83,3 +96,5 @@ with tf.Session() as sess:
                                                          Y: teY[test_indices],
                                                          p_keep_conv: 1.0,
                                                          p_keep_hidden: 1.0})))
+                                                         
+print("tensorboard --logdir=/root/test4/Study_TensorFlow/08\ -\ CNN  --port 6006")
